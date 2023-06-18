08959d362ba0b1d4542108a4e7ed34bd0d53755f
Rohit Khandelwal <56815686+rohitk2001@users.noreply.github.com>
2023-06-18 20:43:41 +0530

    Update Server.jsx

--- src/api/Server.jsx
+++ src/api/Server.jsx
@@ -1,2 +1,2 @@
-const base_url = "https://incident-management-backend-production.up.railway.app/";
+const base_url = "https://incident-management-backend-production.up.railway.app";
 export default base_url;
